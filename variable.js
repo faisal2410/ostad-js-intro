acccb7e216db18e049ef9c6c545434e196684407
Faisal ahmed <faisal2410@yahoo.com>
2022-10-19 13:02:37 +0600

    initial commit

---
+++ variable.js
@@ -0,0 +1,30 @@
+var studentAge=25;
+var studentMarks=85;
+// // 1. variable name can not be any keywords
+// var false = 96;
+// var return = true;
+
+// // 2. variable name has to be in one work. No space
+// var my home address = "New California";
+
+// // 3. variable name can not have quotation
+// var "name" = "Tom Hanks";
+
+// // 4. variable name can not starts with a number but can ends with a number
+// var 99Club = 1964;
+// var club25 = 2025;
+
+// // 5. How to use long names
+// // can not use dash
+// var user-name = "raj bappa"; 
+
+var institueName='Ostad';
+var itemPrice=15000;
+var mobilePrice=20000;
+
+// number string boolean
+var numberOfVoters=15000; //number
+var organizationName="Ostad app";
+var movieName="hawa hawa";
+var isStudent =true;
+var isTeacher=false;
